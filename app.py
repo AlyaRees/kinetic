ba5fe3f1428ebbc254fe90b9ed1b327e26a6ff8c
KurtAntAbano <ka04122005@gmail.com>
2026-01-22 20:44:48 +0000

    Moved index.html to template folder (needed for flask to work)
    app.py includes functions to create the db and a function to Select all and return as a list, which index.html uses to display all current clients.

--- app.py
+++ app.py
@@ -1,17 +1,59 @@
-from flask import Flask, render_template , request, redirect, session, url_for
+from flask import Flask, render_template, request, redirect, session, url_for
 import sqlite3
 
 app = Flask(__name__)
-DB_NAME = "database.db"
+DB_PATH = "clients.db"
 
-def select_all_clients() {
-    
-}
+# ========================= DATABASE FUNCTIONS ========================= #
 
-def search_for_client() {
+def create_clients_table():     # Creates table 
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS clients (
+            ClientID INTEGER PRIMARY KEY AUTOINCREMENT,
+            Name TEXT NOT NULL,
+            Level TEXT NOT NULL,
+            Age INTEGER NOT NULL,
+            Gender TEXT NOT NULL
+        )
+    """)
+    conn.commit()
+    conn.close()
 
-}
 
-def add_client() {
+def get_all_clients():      # uses SELECT query and returns all clients as a list (tuple)
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM clients")
+    clients = cursor.fetchall()
+    conn.close()
+    return clients
 
-}
+
+
+# ========================= FLASK ROUTES ========================
+
+
+@app.route('/')     # automatically executes this function when the program runs
+def index():
+    clients = get_all_clients()     # function that returns list of all users is passed as a parameter
+    return render_template('index.html', clients=clients)
+
+
+# ========================= Run App ========================= #
+
+# import os
+
+if __name__ == '__main__':
+    create_clients_table()
+
+    # if os.environ.get("WERKZEUG_RUN_MAIN") == "true": 
+    #     add_client("Alice", "Beginner", 25, "Female")
+    #     add_client("Ben", "Intermediate", 32, "Male")
+    #     add_client("Chloe", "Advanced", 29, "Female")
+    #     add_client("David", "Beginner", 40, "Male")
+    #     add_client("Ella", "Intermediate", 22, "Female")
+
+    app.run(debug=True)
+
